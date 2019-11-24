4578912dbc55b2db545e750900953b3094817567
thibba <thibba@yahoo.com.br>
2019-11-23 21:28:21 -0300

    Commit Inicial

---
+++ Index.php
@@ -0,0 +1,29 @@
+<?php
+    require('Shared\layout_header.php');
+?> 
+    <div class="container-fluid">
+        <div class="col"></div>
+            <ul class="nav p-3 mb-2 bg-dark text-warning">
+                <li class="nav-item text-warning">
+                    <a class="nav-link active text-warning" href="#">Ativo</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-warning" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-warning" href="#">Link</a>
+                </li>
+                <li class="nav-item text-warning">
+                    <a class="nav-link disabled text-warning" href="#">Desativado</a>
+                </li>
+            </ul
+            <h1>Teste Include BootStrap</h1>
+        </div>
+        <div class="teste">
+            <h1 class="teste">Teste de link Css</h1>
+        </div>
+            
+    </div>
+<?php
+    require('Shared\layout_footer.php');
+?>  
